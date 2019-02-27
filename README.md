90f1747f74e7515c8f3b004eaebf97d1370cdb56
Alexey Ryabov <ar@decast.com>
2019-02-27 10:54:14 +0300

    Solve task

--- README.md
+++ README.md
@@ -1,6 +1,22 @@
 # Github Trends
 
-[TODO. There will be project description]
+The script finds all the github repositories created during the last week, selects the 20 with the most stars and displays the number of open issues for each.
+
+# Example of usage
+
+```bash
+$ python github_trending.py
+
+Github trending
+
+*2001 subsync: Automagically synchronize subtitles with video.
+ URL: https://github.com/smacke/subsync
+ Opened issues: 2
+*1769 frontend-bootcamp: Frontend Workshop from HTML/CSS/JS to TypeScript/React/Redux
+ URL: https://github.com/Microsoft/frontend-bootcamp
+ Opened issues: 2
+...
+```
 
 # Project Goals
 
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,61 @@
+import requests
+from datetime import datetime, timedelta
+
+
+GITHUB_API_URL = "https://api.github.com"
+
+
+def github_search_repos(q, sort="stars", order="desc", page=1, per_page=20):
+    endpoint = "{}{}".format(GITHUB_API_URL, "/search/repositories")
+    payload = {
+        "q": q,
+        "sort": sort,
+        "order": order,
+        "page": page,
+        "per_page": per_page
+    }
+    search_results = requests.get(endpoint, payload)
+    return search_results
+
+
+def github_get_repo_issues(owner, repo, state="all"):
+    endpoint = "{}{}".format(GITHUB_API_URL, "/repos/{}/{}/issues".format(
+        owner,
+        repo
+    ))
+    payload = {
+        "state": state
+    }
+    issues = requests.get(endpoint, payload)
+    return issues.json()
 
 
 def get_trending_repositories(top_size):
-    pass
+    last_week = (datetime.now() - timedelta(days=7)).strftime('%Y-%m-%d')
+    query = "created:>={}".format(last_week)
+    repos = github_search_repos(query, per_page=top_size)
+    return repos.json()["items"]
 
 
 def get_open_issues_amount(repo_owner, repo_name):
-    pass
+    return len(github_get_repo_issues(repo_owner, repo_name, "open"))
 
-if __name__ == '__main__':
-    pass
+
+def main():
+    repos = get_trending_repositories(20)
+    print("Github trending\n")
+    for repo in repos:
+        # owner_name = repo["owner"]["login"]
+        repo_name = repo["name"]
+        desc = repo["description"]
+        url = repo["html_url"]
+        stars = repo["stargazers_count"]
+        # open_issues_count = get_open_issues_amount(owner_name, repo_name)
+        open_issues_count = repo["open_issues"]
+        print("*{} {}: {} ".format(stars, repo_name, desc))
+        print(" URL: {}".format(url))
+        print(" Opened issues: {}".format(open_issues_count))
+
+
+if __name__ == "__main__":
+    main()
